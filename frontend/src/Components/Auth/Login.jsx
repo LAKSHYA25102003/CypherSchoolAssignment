07ee20c0401d14fb7465cb053754971ad8def2aa
LAKSHYA <llakshya63@gmail.com>
2023-04-08 21:09:03 +0530

    small bug is fixed

--- frontend/src/Components/Auth/Login.jsx
+++ frontend/src/Components/Auth/Login.jsx
@@ -58,7 +58,7 @@ const Login = () => {
                         name="email"
                         id="email"
                         className=" border border-gray-300  sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Name"
+                        placeholder="name@company.com"
                         required
                       />
                     </div>
